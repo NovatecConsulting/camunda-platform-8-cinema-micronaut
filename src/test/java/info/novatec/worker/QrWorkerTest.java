ab7c042e73d060d73feb7a60f3cecfd984b4582b
Stefan Schultz <28778796+itsmestefanjay@users.noreply.github.com>
2021-12-10 13:44:12 +0100

    refine imports

--- src/test/java/info/novatec/worker/QrWorkerTest.java
+++ src/test/java/info/novatec/worker/QrWorkerTest.java
@@ -2,7 +2,6 @@ package info.novatec.worker;
 
 import info.novatec.process.Variables;
 import info.novatec.service.QRCodeService;
-import org.assertj.core.api.Assertions;
 import org.camunda.community.zeebe.testutils.stubs.ActivatedJobStub;
 import org.camunda.community.zeebe.testutils.stubs.JobClientStub;
 import org.junit.jupiter.api.BeforeEach;
@@ -39,7 +38,8 @@ class QrWorkerTest {
     @Test
     void test_worker_completes_job_and_sets_it_as_var_when_qr_code_is_generated() throws Exception {
         // given
-        when(service.generateQRCode(any())).thenReturn("myQrCodeBase64");
+        when(service.generateQRCode(any()))
+                .thenReturn("myQrCodeBase64");
 
         // when
         worker.generateTicket(jobClient, activatedJob);
@@ -55,13 +55,15 @@ class QrWorkerTest {
     @Test
     void test_worker_fails_job_when_qr_code_generation_fails() throws Exception {
         // given
-        when(service.generateQRCode(any())).thenThrow(new IOException("error generating qr code"));
+        when(service.generateQRCode(any()))
+                .thenThrow(new IOException("error generating qr code"));
 
         // when
         worker.generateTicket(jobClient, activatedJob);
 
         // then
-        assertThat(activatedJob).failed()
+        assertThat(activatedJob)
+                .failed()
                 .hasErrorMessage("error generating qr code")
                 .hasRetries(0);
     }
